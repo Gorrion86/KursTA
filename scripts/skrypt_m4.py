48d83f71899dd028f8398a7eb7b9f75752daac2f
Gorrion <trust.no1GitCode>
2021-04-18 21:20:55 +0200

    kalkulator

---
+++ scripts/skrypt_m4.py
@@ -0,0 +1,12 @@
+licznik = 0
+
+# while True:
+#     print(licznik)
+#     licznik += 1
+#     if licznik >=8:
+#         break
+
+for x in range(20):
+    if x % 2 == 0:
+        continue
+    print(x)
